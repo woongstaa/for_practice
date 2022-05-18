a25b967bebb0879f6eadddcbe02409ac84a17056
woongstaa <jinung91@gmail.com>
2022-05-18 18:37:57 +0900

    Add: redux_middleware_practice initial setting

---
+++ redux_middleware_practice/src/containers/CounterContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
+
+export default function CounterContainer() {
+  const number = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => {
+    dispatch(increase());
+  };
+
+  const onDecrease = () => {
+    dispatch(decrease());
+  };
+
+  return <Counter number={number} onDecrease={onDecrease} onIncrease={onIncrease} />;
+}
